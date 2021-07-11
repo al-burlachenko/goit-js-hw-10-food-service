c5df3c10e553dbdf12088944f314f4db1393af5a
al-burlachenko <alexandrburl@gmail.com>
2021-07-11 22:00:34 +0300

    Подключает файл normalize, входящие данные, шаблон. Реализует функционал переключения цвета

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+import "../node_modules/normalize.css/normalize.css";
+
+import upcominEvents from './menu.json';
+import eventsTemplate from './templates/events.hbs';
 import './sass/main.scss';
+
+// console.log(eventsTemplate({ items: upcominEvents }));
+
+const menuBoard = document.querySelector('.js-menu');
+const themeToggle = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+themeToggle.addEventListener('change', onThemeChange);
+
+body.classList.add(Theme.LIGHT);
+menuBoard.innerHTML = eventsTemplate({ items: upcominEvents });
+
+function onThemeChange(event) {
+  console.log(themeToggle.checked);
+  if (themeToggle.checked) {
+    return body.classList.replace(Theme.LIGHT, Theme.DARK);
+  }
+  return body.classList.replace(Theme.DARK, Theme.LIGHT);
+}
